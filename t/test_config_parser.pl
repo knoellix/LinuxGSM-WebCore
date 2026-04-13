2f52416c63d414ef08b4c49754291b2c8a2b51ed
Christian Möllmann <moellix@knoellix.net>
2026-04-13 12:06:15 +0200

    test: add failing tests for layered config parser
    Tests verify that _parse_lgsm_config reads config-default/ as base layer
    and that config-lgsm/common.cfg overrides defaults. Currently fails because
    the parser does not yet read config-default/.

---
+++ t/test_config_parser.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+# t/test_config_parser.pl
+use strict;
+use warnings;
+use Test::More tests => 6;
+use File::Temp qw(tempdir);
+use FindBin qw($Bin);
+use lib "$Bin/..";
+
+sub sanitize_input { my ($s) = @_; $s =~ s/[^a-zA-Z0-9_-]//g; return $s; }
+sub error          { die "error: $_[0]\n" }
+sub system_logged  { return 0 }
+sub firewall_status { return 0 }
+
+our %text = ();
+
+require 'src/lib/instance.pl';
+
+# Test-Verzeichnis-Struktur:
+# $dir/
+#   lgsm/
+#     config-default/
+#       _default.cfg          (port=25565, gamename=Minecraft)
+#     config-lgsm/
+#       common.cfg            (port=9999)
+
+# Test 1-3: Nur config-default vorhanden -> _has_user_config = 0
+{
+    my $dir = tempdir(CLEANUP => 1);
+    mkdir "$dir/lgsm";
+    mkdir "$dir/lgsm/config-default";
+    open(my $fh, '>', "$dir/lgsm/config-default/_default.cfg") or die $!;
+    print $fh "port=\"25565\"\n";
+    print $fh "gamename=\"Minecraft\"\n";
+    close($fh);
+
+    my %cfg = _parse_lgsm_config($dir, 'mcserver');
+    is($cfg{port},             '25565',    'default port read from config-default');
+    is($cfg{gamename},         'Minecraft','default gamename read from config-default');
+    is($cfg{_has_user_config}, 0,          'no user config -> _has_user_config is 0');
+}
+
+# Test 4-6: config-lgsm/common.cfg überschreibt default
+{
+    my $dir = tempdir(CLEANUP => 1);
+    mkdir "$dir/lgsm";
+    mkdir "$dir/lgsm/config-default";
+    mkdir "$dir/lgsm/config-lgsm";
+
+    open(my $fh, '>', "$dir/lgsm/config-default/_default.cfg") or die $!;
+    print $fh "port=\"25565\"\n";
+    print $fh "gamename=\"Minecraft\"\n";
+    close($fh);
+
+    open($fh, '>', "$dir/lgsm/config-lgsm/common.cfg") or die $!;
+    print $fh "port=\"9999\"\n";
+    close($fh);
+
+    my %cfg = _parse_lgsm_config($dir, 'mcserver');
+    is($cfg{port},             '9999',     'common.cfg overrides default port');
+    is($cfg{gamename},         'Minecraft','gamename still from default when not overridden');
+    is($cfg{_has_user_config}, 1,          'common.cfg exists -> _has_user_config is 1');
+}
